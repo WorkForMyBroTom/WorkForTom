b21c69d50692d69ab1ee5387945beb213f1dbf78
WorkForMyBroTom <121464076+WorkForMyBroTom@users.noreply.github.com>
2022-12-29 00:17:31 +0200

    Add files via upload

---
+++ mywork.js
@@ -0,0 +1,6 @@
+let addMessage = document.querySelector('.message')
+addButton= document.querySelector('.add')
+
+addButton.addEventListener('click', function(){
+   console.log(addMessage.value);
+})
